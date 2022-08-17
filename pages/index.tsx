640ff8c6be827a2192571732406cc8bbaff8d23d
Myagmarsuren <myagmarsurenborkhuu@gmail.com>
2022-08-17 19:40:55 +0800

    add tailwind prettier plugin, apply eslint rules

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div>
-      <p className="text-6xl font-bold text-cooler-black">lmaooo</p>
+      <p className="text-6xl font-bold  text-cooler-black">lmaooo</p>
     </div>
   );
 };
