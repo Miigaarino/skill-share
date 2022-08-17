510497fbe929587af88d5bc54ac8bf6263a5218d
Myagmarsuren <myagmarsurenborkhuu@gmail.com>
2022-08-17 20:41:32 +0800

    add dark mode

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html className="h-full bg-cool-gray antialiased" lang="en">
       <Head />
-      <body className="flex h-full flex-col">
+      <body className="flex h-full flex-col duration-300 dark:bg-cool-black">
         <Main />
         <NextScript />
       </body>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,28 @@
 import type { NextPage } from "next";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 
 const Home: NextPage = () => {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  const { theme, setTheme } = useTheme();
+
   return (
     <div>
-      <p className="text-6xl font-bold  text-cooler-black">lmaooo</p>
+      <button
+        type="button"
+        onClick={() => setTheme(theme === "light" ? "dark" : "light")}
+        className="text-cooler-black dark:text-cool-gray"
+      >
+        SSIWTCH
+      </button>
+      <p className="text-6xl font-bold text-cooler-black dark:text-cool-gray">
+        {mounted && theme}
+      </p>
     </div>
   );
 };
